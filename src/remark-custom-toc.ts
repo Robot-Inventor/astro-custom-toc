909559f758021690ce7185fb1e9a16cc423778a2
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2024-03-17 02:30:48 +0900

    feat: use ESLint #12 (#15)
    * feat: use ESLint #12
    
    * fix: fix lint error

--- src/remark-custom-toc.ts
+++ src/remark-custom-toc.ts
@@ -1,7 +1,8 @@
-import type { AstroUserConfig } from "astro";
-import { visit } from "unist-util-visit";
-import Slugger from "github-slugger";
+import type { Html, Node, Nodes, Root } from "mdast";
 import { JSDOM } from "jsdom";
+import type { Plugin } from "unified";
+import Slugger from "github-slugger";
+import { visit } from "unist-util-visit";
 
 interface Heading {
     slug: string;
@@ -17,37 +18,103 @@ export interface RemarkCustomTocOptions {
     ordered?: boolean;
 }
 
-type RemarkPlugin = NonNullable<NonNullable<AstroUserConfig["markdown"]>["remarkPlugins"]>[number];
-
 interface VFile {
     data: {
         astro?: {
-            frontmatter: Record<string, any>;
+            frontmatter: Record<string, unknown>;
         };
     };
 }
 
-interface CommentNode {
+interface Comment extends Node {
     type: "comment";
     value: "";
     commentValue: string;
 }
 
-const defaultTemplate = (html: string) => {
-    return `
+declare module "mdast" {
+    interface RootContentMap {
+        comment: Comment;
+    }
+
+    interface Data {
+        id: string;
+    }
+}
+
+const defaultTemplate = (html: string): string =>
+    `
 <aside class="toc">
     <h2>Contents</h2>
     <nav>
         ${html}
     </nav>
 </aside>`.trim();
+
+const defaultOptions = {
+    maxDepth: 3,
+    ordered: false,
+    template: defaultTemplate
+} as const satisfies Required<RemarkCustomTocOptions>;
+
+const isTocCommentNode = (node: Nodes): node is Comment =>
+    node.type === "comment" && node.commentValue.trim().toLowerCase() === "toc";
+
+// eslint-disable-next-line max-statements
+const generateToc = (options: Required<RemarkCustomTocOptions>, headings: Heading[]): Html => {
+    const { document } = new JSDOM().window;
+
+    const toc: HTMLElement = document.createElement(options.ordered ? "ol" : "ul");
+    let currentDepth = headings[0].depth;
+    let currentParent = toc;
+    for (const heading of headings) {
+        // eslint-disable-next-line no-continue
+        if (heading.depth > options.maxDepth) continue;
+
+        if (heading.depth === currentDepth) {
+            const li = document.createElement("li");
+            const link = document.createElement("a");
+            link.href = `#${heading.slug}`;
+            link.textContent = heading.text;
+            li.appendChild(link);
+            currentParent.appendChild(li);
+            currentDepth = heading.depth;
+        } else if (heading.depth > currentDepth) {
+            const ul = document.createElement(options.ordered ? "ol" : "ul");
+            const li = document.createElement("li");
+            const link = document.createElement("a");
+            link.href = `#${heading.slug}`;
+            link.textContent = heading.text;
+            li.appendChild(link);
+            ul.appendChild(li);
+            currentParent.appendChild(ul);
+            currentParent = ul;
+            currentDepth = heading.depth;
+        } else {
+            // eslint-disable-next-line id-length
+            for (let i = 0; i < currentDepth - heading.depth; i++) {
+                currentParent = currentParent.parentElement!;
+            }
+            const li = document.createElement("li");
+            const link = document.createElement("a");
+            link.href = `#${heading.slug}`;
+            link.textContent = heading.text;
+            li.appendChild(link);
+            currentParent.appendChild(li);
+            currentDepth = heading.depth;
+        }
+    }
+
+    return {
+        type: "html",
+        value: options.template(toc.outerHTML)
+    };
 };
 
-export const remarkCustomToc: RemarkPlugin = ({
-    template = defaultTemplate,
-    maxDepth = 3,
-    ordered = false
-}: RemarkCustomTocOptions = {}) => {
+export const remarkCustomToc: Plugin<[RemarkCustomTocOptions], Root> = (userOptions: RemarkCustomTocOptions) => {
+    const options = { ...defaultOptions, ...userOptions };
+
+    // eslint-disable-next-line max-statements
     return (tree, { data }: VFile) => {
         if (data.astro && data.astro.frontmatter.showToc !== true) return;
 
@@ -57,81 +124,26 @@ export const remarkCustomToc: RemarkPlugin = ({
         const headings: Heading[] = [];
         visit(tree, "heading", (node): void => {
             const { depth } = node;
-            const text = node.children.map((n) => ("value" in n ? n.value : "")).join("");
+            // eslint-disable-next-line no-extra-parens
+            const text = node.children.map((children) => ("value" in children ? children.value : "")).join("");
             const slug = slugs.slug(text);
-            // @ts-ignore
             node.data = { id: slug };
-            headings.push({ slug, text, depth });
+            headings.push({ depth, slug, text });
         });
 
         let tocIndex = 0;
-        visit(tree, "comment", (node: CommentNode, index) => {
-            if (node.commentValue.trim().toLowerCase() === "toc" && index !== undefined) {
+        visit<Root, string>(tree, "comment", (node, index) => {
+            if (isTocCommentNode(node) && typeof index !== "undefined") {
                 tocIndex = index;
             }
         });
 
-        visit(tree, "html", (node, index) => {
-            if (node.value.trim().toLowerCase() === "<!-- toc -->" && index !== undefined) {
-                tocIndex = index;
-            }
-        });
-
-        const toc = generateToc(template, maxDepth, ordered, headings);
-        if (tocIndex === 0) {
-            // @ts-ignore
-            tree.children.unshift(toc);
-        } else {
-            // @ts-ignore
+        const toc = generateToc(options, headings);
+        if (tocIndex) {
+            // eslint-disable-next-line no-magic-numbers
             tree.children.splice(tocIndex, 1, toc);
-        }
-    };
-};
-
-const generateToc = (template: RemarkCustomTocTemplate, maxDepth: number, ordered: boolean, headings: Heading[]) => {
-    const { document } = new JSDOM().window;
-
-    const toc: HTMLElement = document.createElement(ordered ? "ol" : "ul");
-    let currentDepth = headings[0].depth;
-    let currentParent = toc;
-    for (const heading of headings) {
-        if (heading.depth > maxDepth) continue;
-
-        if (heading.depth === currentDepth) {
-            const li = document.createElement("li");
-            const a = document.createElement("a");
-            a.href = `#${heading.slug}`;
-            a.textContent = heading.text;
-            li.appendChild(a);
-            currentParent.appendChild(li);
-            currentDepth = heading.depth;
-        } else if (heading.depth > currentDepth) {
-            const ul = document.createElement(ordered ? "ol" : "ul");
-            const li = document.createElement("li");
-            const a = document.createElement("a");
-            a.href = `#${heading.slug}`;
-            a.textContent = heading.text;
-            li.appendChild(a);
-            ul.appendChild(li);
-            currentParent.appendChild(ul);
-            currentParent = ul;
-            currentDepth = heading.depth;
         } else {
-            for (let i = 0; i < currentDepth - heading.depth; i++) {
-                currentParent = currentParent.parentElement!;
-            }
-            const li = document.createElement("li");
-            const a = document.createElement("a");
-            a.href = `#${heading.slug}`;
-            a.textContent = heading.text;
-            li.appendChild(a);
-            currentParent.appendChild(li);
-            currentDepth = heading.depth;
+            tree.children.unshift(toc);
         }
-    }
-
-    return {
-        type: "html",
-        value: template(toc.outerHTML)
     };
 };
