d013359b6c9c4c34957526187a59c8907f9d5fef
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2024-03-18 00:22:41 +0900

    feat: add JSDoc #17 (#18)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import type { AstroConfig, AstroIntegration } from "astro";
 import { RemarkCustomTocOptions, remarkCustomToc } from "./remark-custom-toc.js";
 import remarkComment from "remark-comment";
 
+/**
+ * Check if the order of integrations is correct.
+ * `customToc()` must be loaded before `mdx()`.
+ * @param config Astro config
+ */
 const checkOrder = (config: AstroConfig): void => {
     const customTocIndex = config.integrations.findIndex((integration) => integration.name === "astro-custom-toc");
     const mdxIndex = config.integrations.findIndex((integration) => integration.name === "@astrojs/mdx");
@@ -10,15 +15,21 @@ const checkOrder = (config: AstroConfig): void => {
         throw new Error(
             `
 MDX integration configured before astro-custom-toc.
-\`astroCustomToc()\` must be loaded before \`mdx()\`.
+\`customToc()\` must be loaded before \`mdx()\`.
 `.trim()
         );
     }
 };
 
+/**
+ * Create the astro-custom-toc integration.
+ * @param options Options for the integration
+ * @returns The AstroIntegration object
+ */
 const astroCustomToc = (options?: RemarkCustomTocOptions): AstroIntegration => {
     const integration: AstroIntegration = {
         hooks: {
+            // eslint-disable-next-line jsdoc/require-jsdoc
             "astro:config:setup": ({ config, updateConfig }) => {
                 checkOrder(config);
                 updateConfig({
